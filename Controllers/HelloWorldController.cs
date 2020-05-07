3dec9c983a177817928aec1c7253bec6194f59dc
badr <badroxe.saadz@gmail.com>
2020-05-07 23:20:13 +0000

    First Asp .Net web app (MvcMovie)

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //GET: /HELLOWORLD/welcome
+        public IActionResult Welcome(string name,int numTimes=1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name ="Relese Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName ="decimal(15,2)")]
+        public decimal Price { get; set; }
+    }
+}
